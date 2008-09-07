f7146f773a1813eb5dc705469863aea5de763b60
Joachim Bengtsson <joachimb@gmail.com>
2008-09-07 17:01:54 +0000

    Cleaning up

---
+++ Classes/Other Sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MobileOverload
+//
+//  Created by Joachim Bengtsson on 2008-08-12.
+//  Copyright Third Cog Software 2008. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+}
